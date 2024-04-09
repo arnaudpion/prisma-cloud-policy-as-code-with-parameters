938509cc0b342a67318512bcae12730219ef8de7
Arnaud Pion <apion@paloaltonetworks.com>
2024-04-09 09:27:26 +0200

    Deleted 'Build' Policy

--- config_policy.tf
+++ config_policy.tf
@@ -2,7 +2,7 @@ resource "prismacloud_policy" "test_custom_awstags" {
   name            = "AWS Instance contains the forbidden 'env' key"
   policy_type     = "config"
   cloud_type      = "aws"
-  policy_subtypes = ["run", "build"]
+  policy_subtypes = ["run"]
   severity        = "low"
   labels          = ["test-custom"]
   description     = "No AWS instance should have the env key!"
@@ -12,16 +12,8 @@ resource "prismacloud_policy" "test_custom_awstags" {
     rule_type = "Config"
     parameters = {
       savedSearch = true
-      withIac     = true
     }
     criteria = prismacloud_saved_search.test-rql-awsenvkey.id
-    children {
-      type           = "build"
-      recommendation = "fix it"
-      metadata = {
-        "code" : file("build_policies/aws_env_tag.yaml"),
-      }
-    }
   }
   compliance_metadata {
     compliance_id = prismacloud_compliance_standard_requirement_section.test_custom_complaince_requirements_section_AWS.csrs_id
